a458fbeda96f30d7b74062b1f602f4adc3208d3a
Apostolos D. Masiakos <amasiakos@gmail.com>
2016-03-05 02:17:53 +0200

    Added licensing information. Added short description in README.md.

---
+++ README.md
@@ -0,0 +1,30 @@
+=*= packetCraft =*=
+    _____________
+   /\-//+/|\+\\-/\
+  //+\/--/-\--\/+\\
+  |/\|+//_|_\\+|/\|
+  \\+\-\-\-/-/-/+//
+   \\/\+\-|-/+/\//
+    \\/\_\_/_/\//
+     \\/\+|+/\//
+      \//\|/\\/
+       \/\+/\/
+        \\|//
+         \+/
+          ^
+
+This tool, intends to aid people who develop applications and are in need for
+various packet creation schemes.
+
+--Supported packet formats:
+(a) Packet schemes as they are specified in the CCSDS 203.0-B-2, published from
+The Consultative Committee for Space Data Systems (CCSDS)
+(for more info look in: http://public.ccsds.org/).
+
+--To do (in order of necessity)
+(a) Add socket packet transmission functionality.
+(b) Add more packet schemes.
+(c) Add a Graphical User Interface (GUI).
+(d) Add support for extra packet serialization formats.
+
+(proudly crafted with the ruby programming language)  
--- lib/utils.rb
+++ lib/utils.rb
@@ -1,3 +1,45 @@
+# packetCraft.
+#
+# Copyright (C) 2016 by Apostolos D. Masiakos (amasiakos@gmail.com)
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#  *  Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#
+#  *  Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+
 FRAME_DEL_H = 0x7E
 FRAME_DEL_B = 0b01111110
 FRAME_DEL_B_A = Array.[](0,1,1,1,1,1,1,0)
@@ -13,30 +55,43 @@ def ClaimSerialPort( serialline )
     #Spawn the serial line listener thread.
     #a call to Thread.sleep() is not needed because the
     #loop is blocking at its own, on the Serial.read() call.
-#    x = Thread.new() {      
+    x = Thread.new() {      
 #      $serialPort2 = SerialPort.new(serialline, 9600, 8, 1, SerialPort::NONE);
-#      $serialPort2.read_timeout=0; #read data as soon they appear on the rx line.
-#      dataA = Array.new();
-#      loop do #keep polling the rx line.
-##        printf("\nbefore\n");
-#        incmData = $serialPort2.read($serialPort.data_bits);
-##        incmData = $serialPort.read(1);
-#        if incmData.unpack("C*").eql?(FRAME_DEL_B_A)
-#          printf("\nDISCARING FRAME\n");
-#        else
-#          printf("#{incmData.unpack("C*")}");
-#        end
-##        $serialPort.flush_input();
-##        $serialPort.flush_output();
-#        
-##        printf("\n#{incmData}\n");
+      $serialPort.read_timeout=0; #read data as soon they appear on the rx line.
+      dataA = Array.new();
+      frameseen=0;
+      loop do #keep polling the rx line.
+#        printf("\nbefore\n");
+        incmData = $serialPort.read($serialPort.data_bits);
+#        incmData = $serialPort.read(1);
+        printf("\nthe read data are:#{incmData.unpack("C*")}\n");
+        if incmData.unpack("C*").eql?(FRAME_DEL_B_A);
+          printf("\nDISCARING FRAME\n");
+          frameseen+=1;
+#        elsif incmData.unpack("C*").eql?(FRAME_ESCAPE_B_A);
+        else
+          if frameseen==2
+            printf("\n\nThe received message is#{ byteDestuff( Array.new(dataA))}\n\n");
+            dataA.clear;
+            frameseen=0;
+          else
+            incmData.unpack("C*").each{ |elem|
+              dataA.push(elem);
+              printf("\ndata in dataArray are:#{data.inspect}")
+            }
+          end
+          
+        end
+#        $serialPort.flush_input();
+#        $serialPort.flush_output();
+#        printf("\n#{incmData}\n");
+      end
+#      $serialPort2 = Serial.new
+#      while true
+#        puts "func1 at: #{Time.now}"
+#        sleep(1);
 #      end
-##      $serialPort2 = Serial.new
-##      while true
-##        puts "func1 at: #{Time.now}"
-##        sleep(1);
-##      end
-#    }; x.run;
+    }; x.run;
   rescue
     $serialPort= nil;
     print("Serial port (or at least something emulating) is not availiable on this system,"\
@@ -233,6 +288,7 @@ end
 #the reverse of byte stuffing.
 #detect the escape sequences in the data and discard them.
 #then shift (invert) the fifth bit of the next octet.
+#the given array is already without the header/tail frame delimeters
 def byteDestuff( array )  
 # frame delimeters 0x7E, 0xb01111110, '~'
 # escape delimeter 0x7D, 0xb1111101, '}'
@@ -240,13 +296,12 @@ def byteDestuff( array )
 #FRAME_DEL_B = 0b01111110
 #FRAME_ESCAPE_H = 0x7D
 #FRAME_ESCAPE_B = 0b01111101
-
   pos = 0;
 #  printf("\nArray before destuff is:\n #{array}, with size: #{array.length}\n");
   #delete the header flag
-    array.slice!(0,8);
+#    array.slice!(0,8);
   #delete the tail flag
-    array.slice!(array.length-8, 8);
+#    array.slice!(array.length-8, 8);
 #  printf("\nArray after flag destuff is:\n #{array}, with size: #{array.length}\n");
   
 while pos+7 <= array.length do
